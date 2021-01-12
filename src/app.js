68adaf1a58fd80f502fb043ed60548f6f5f89ddc
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-12 14:49:48 +0000

    connected to API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("hello");
+new Vue({
+  el: '#app',
+  data: {
+    countries: []
+  },
+  mounted: function () {
+    this.fetchCountries();
+
+  },
+  methods: {
+    fetchCountries: function (){
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then( data => this.countries = data );
+    }
+  },
+})
 });
